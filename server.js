478feb2b387b4d681b95c5facddb300858bf0cfa
Rangel Almeida <rangel@zazz.com.br>
2019-03-04 18:23:46 -0300

    Ajustes auth

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const mongoose = require('./src/database');
+const cors = require('cors');
+const requireDir = require('require-dir');
 
 const app = express();
+app.use(express.json());
+app.use(cors()); //dentro do cors, podemos colocar de quais dominios aceitamos requisicao, etc.
+
+
+require('./src/controllers/authController')(app);
+
+//requireDir('./src/models')(app);
 
 //mongoose.connect
 
 app.listen(3001);
+ß
--- src/models/User.js
+++ src/models/User.js
@@ -26,3 +26,7 @@ const UserSchema = new mongoose.Schema({
     }    
 
 });
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
